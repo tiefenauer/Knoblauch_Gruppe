1f264f61d413321a9f1a4388ef5897cc9be609bc
Roman <clickagent.ch@gmail.com>
2012-09-07 02:16:00 +0200

    second commit

---
+++ src/main/java/ch/zhaw/knoblauch/gruppe/App.java
@@ -0,0 +1,13 @@
+package ch.zhaw.knoblauch.gruppe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
